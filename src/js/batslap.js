38aba26e009c51d668abfa943f75f47dbcf4fb23
Joe Curlee <joe.curlee@gmail.com>
2015-01-13 13:48:44 -0600

    added ability to dynamically change dialog

--- src/js/batslap.js
+++ src/js/batslap.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Batslap = (function () {
+(function (window) {
   var instance;
 
   var initialize = function () {
@@ -10,34 +10,35 @@ var Batslap = (function () {
     for (i = 0; i < elements.length; i++) {
       addDialog(elements[i]);
     }
-  }
+  };
 
   var addDialog = function (obj) {
     var items = ['rbn', 'bmn'];
     var item;
     var data;
     var i;
-    
+
     for (i = 0; i < items.length; i++) {
       item = items[i];
       data = obj.dataset[item];
       if (data) {
-        setBalloon(obj, item);
-        obj.appendChild(
-          dialog(parseText(data), item)
-        );
+        setBalloon(obj, item, data);
       }
     }
-  }
+  };
 
-  var setBalloon = function (item, className) {
+  var setBalloon = function (item, className, data) {
     var span = document.createElement('span');
     span.className = 'wordballoon wordballoon-' + className;
 
     item.appendChild(
       span
     );
-  }
+
+    item.appendChild(
+      dialog(parseText(data), className)
+    );
+  };
 
   var dialog = function (txt, className) {
     var span = document.createElement('span');
@@ -47,7 +48,7 @@ var Batslap = (function () {
     p.className = className;
     p.appendChild(span);
     return p;
-  }
+  };
 
   var parseText = function (txt) {
     if (txt) {
@@ -55,21 +56,31 @@ var Batslap = (function () {
         .replace(/\*\*(.+)\*\*/, '<strong>$1</strong>')
         .replace(/\*(.+)\*/, '<em>$1</em>');
     }
-  }
+  };
+
+  var changeText = function (txt, className) {
+    var elements = document.getElementsByClassName(className);
+    var i;
+    var elems;
+    for (i = 0; i < elements.length; i++) {
+      elems = elements[i].getElementsByTagName('span');
+      elems[0].innerHTML = parseText(txt);
+    }
+  };
 
   var Batslap = {
+    talk: changeText
   };
+
+  initialize();
 
   return {
     getInstance: function () {
       if (!instance) {
-        initialize();
         instance = Batslap;
       }
       return instance;
     }
   }
 
-})();
-
-window.bs = Batslap.getInstance();
+})(window);
