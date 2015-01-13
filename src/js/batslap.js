62f60447ea0212c0c03dc147f65070fde49abac7
Joe Curlee <joe.curlee@gmail.com>
2015-01-13 15:57:57 -0600

    better handling of dynamically changing text plus added examples to the demo

--- src/js/batslap.js
+++ src/js/batslap.js
@@ -5,44 +5,67 @@ var Batslap = (function () {
 
   function initialize() {
     var elements = document.getElementsByClassName('batslap');
-    var dialogData = [];
-
+    var dialogDom = [];
+    var balloons = [];
     var i;
+
+    // Create dom elements and load data.
     for (i = 0; i < elements.length; i++) {
-      addDialog(elements[i]);
+      addDialog(elements[i], i);
     }
 
-    function addDialog (obj) {
-      var items = ['rbn', 'bmn'];
-      var item;
-      var data;
+    // Adds a word balloon with dialog gathered from data attribute.
+    function addDialog(item, index) {
+      var classNames = ['rbn', 'bmn'];
+      var className;
       var i;
 
-      for (i = 0; i < items.length; i++) {
-        item = items[i];
-        data = obj.dataset[item];
-        if (data) {
-          setBalloon(obj, item, data);
-        }
+      for (i = 0; i < classNames.length; i++) {
+        className = classNames[i];
+
+        // Add a word balloon to the dom.
+        addBalloon(item, className, item.dataset[className], index);
       }
     }
 
-    function setBalloon (item, className, data) {
+    // Adds a word balloon to dom.
+    function addBalloon(item, className, data, index) {
       var span = document.createElement('span');
-      span.className = 'wordballoon wordballoon-' + className;
+      var classNames = ['wordballoon', 'wordballoon-' + className];
+
+      if (!data) {
+        classNames.push('hide');
+      }
+
+      if (!balloons[index]) {
+        balloons[index] = {};
+      }
+
+      span.className = classNames.join(' ');
+
+      balloons[index][className] = span;
 
       item.appendChild(
         span
       );
 
       item.appendChild(
-        dialog(parseText(data), className)
+        addText(parseText(data), className, index)
       );
     }
 
-    function dialog (txt, className) {
+    // Adds text to dom.
+    function addText(txt, className, index) {
       var span = document.createElement('span');
-      span.innerHTML = txt;
+
+      if (txt) {
+        span.innerHTML = txt;
+      }
+
+      if (!dialogDom[index]) {
+        dialogDom[index] = {};
+      }
+      dialogDom[index][className] = span;
 
       var p = document.createElement('p');
       p.className = className;
@@ -50,7 +73,8 @@ var Batslap = (function () {
       return p;
     }
 
-    function parseText (txt) {
+    // Parses text to add strong and emphasis tags.
+    function parseText(txt) {
       if (txt) {
         return txt
           .replace(/\*\*(.+)\*\*/, '<strong>$1</strong>')
@@ -58,16 +82,46 @@ var Batslap = (function () {
       }
     }
 
-    function talk(txt, who, index) {
-      if (index) {
-        dialogData[index][who] = txt;
+    // Change text for all or one instance of batslap.
+    function talk(txt, className, index) {
+      var i;
+      var txt = parseText(txt);
+      var hide = (!txt) ? true : false;
+
+      if (index === false || index === undefined) {
+        for (i = 0; i < dialogDom.length; i++) {
+          changeText(txt, className, i, hide);
+        }
+      } else {
+        changeText(txt, className, index, hide);
       }
-      else {
-        var i;
-        for (i = 0; i < dialogData.length; i++) {
-          dialogData[i][who] = txt;
+    }
+
+    function changeText(txt, className, index, hide) {
+      showHideBalloon(index, className, hide);
+      if (!txt) {
+        txt = '';
+      }
+      dialogDom[index][className].innerHTML = txt;
+    }
+
+    function showHideBalloon(index, className, hide) {
+      var bclass = balloons[index][className].className.split(' ');
+      for (i = 0; i < bclass.length; i++) {
+        if (bclass[i] == 'hide') {
+          if (!hide) {
+            bclass[i] = undefined;
+          } else {
+            hide = false;
+          }
         }
       }
+
+      if (hide) {
+        bclass.push('hide');
+      }
+
+      balloons[index][className].className = bclass.join(' ');
     }
 
     return {
@@ -78,7 +132,7 @@ var Batslap = (function () {
       btalk: function (txt, index) {
         talk(txt, 'bmn', index);
       }
-    }
+    };
   }
 
   return {
@@ -88,7 +142,7 @@ var Batslap = (function () {
       }
       return instance;
     }
-  }
+  };
 
 })();
 
